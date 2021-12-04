d504e7e87e0deb0fe715cd78a2f5097cca43ea7a
Nhi Dinh <tuyetnhi1607@github.com>
2021-12-05 00:46:15 +0700

    corsss

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const corsOptions ={
-    origin:'https://tuyetnhi1607-cms-website.herokuapp.com/', 
+    origin:'*', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200,
  }
