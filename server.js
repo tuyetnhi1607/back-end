3ac1b913cc82df8a5635c286ba048c8abe063aa8
Nhi Dinh <tuyetnhi1607@github.com>
2021-12-04 23:24:25 +0700

    cors

--- server.js
+++ server.js
@@ -10,20 +10,17 @@ const postRoute = require('./routes/postRoute');
 const express = require('express');
 const cors = require('cors');
 const app = express();
-app.use(cors());
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+ 
+ app.use(cors(corsOptions)) 
 app.use(express.json());
 // //mount the routes to
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/post', postRoute);
-
-// app.get('/', (req, res, next) => {
-//     res.status(200).json({
-//         status: 'success',
-//         data: {
-//             ports: []
-//         }
-//     })
-// })
 
 const port = 5000;
 
